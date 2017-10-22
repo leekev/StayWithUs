e08033472343abdd566d38235d203330b91dc7df
khmermenten <32060071+khmermenten@users.noreply.github.com>
2017-10-22 13:12:37 -0700

    thicc

--- src/PatternGenerator.java
+++ src/PatternGenerator.java
@@ -1,38 +1,53 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class PatternGenerator {
-	public PatternGenerator(int number)
+	public PatternGenerator()
 	{
-		
-		if (number == 0)
-			zero();
+
 	}
-	
-	public void zero()
+
+	public int run(int number, Set<Integer> set)
 	{
-		
+		//if (number == 0)
+			return zero(set);
+//		if (number == 1)
+//			return one(set);
+//		if (number == 2)
+//			return two(set);
+//		if (number == 3)
+//			return three(set);
+//		return four(set);
 	}
-	
-	public void one()
+
+	public int zero(Set<Integer> set)
 	{
-		
+		int random = (int) (Math.random() * 100);
+		int hash = Integer.toString(random).hashCode();
+		set.add(hash);
+		set.add(hash/2);
+		set.add(hash * 2);
+		set.add(hash/4);
+		set.add(hash * 4);
+		set.add(hash/8);
+		return hash*8;
 	}
-	public void two()
-	{
-		
-	}
-	public void three()
-	{
-		
-	}
-	public void four()
-	{
-		
-	}
-	
-	public void five()
-	{
-		
-	}
-	
+
+//	public int one(Set<Integer> set)
+//	{
+//		
+//
+//	}
+//	public int two(Set<Integer> set)
+//	{
+//
+//	}
+//	public int three(Set<Integer> set)
+//	{
+//
+//	}
+//	public int four(Set<Integer> set)
+//	{
+//
+//	}
 }
