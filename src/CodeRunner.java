85a09059032cb20d796d2a5535bb96b43763b5dd
Bryan Van Draanen <bryanvd@cs.washington.edu>
2017-10-21 22:31:36 -0700

    crazy ass compiler lit

---
+++ src/CodeRunner.java
@@ -0,0 +1,56 @@
+import java.net.URI;
+import java.util.Arrays;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.ToolProvider;
+import javax.tools.JavaFileObject.Kind;
+import javax.tools.SimpleJavaFileObject;
+
+public class CodeRunner {
+    
+    private static String DIR;
+
+    JavaCompiler compiler;
+    
+    public CodeRunner() {
+        compiler = ToolProvider.getSystemJavaCompiler();
+        CodeRunner.class.getClassLoader();
+        CodeRunner.DIR = ClassLoader.getSystemResource(".").getPath();
+    }
+    
+    public boolean runCode(String code) {
+        JavaFileObject playerCode = new JavaSourceFromString(DIR + "PlayerCode", code);
+        Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(playerCode);
+        
+        CompilationTask task = compiler.getTask(null, null, null, null, null, compilationUnits);
+        
+        boolean compilationSuccess = task.call();
+        
+        if (compilationSuccess) {
+            try {
+                Class.forName("StayWithUs").getDeclaredMethod("run", String.class).invoke(null, "WhiteMorning");
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
+class JavaSourceFromString extends SimpleJavaFileObject {
+    private final String code;
+    
+    public JavaSourceFromString(String name, String code) {
+        super(URI.create("string:///" + name.replace('.', '/') + Kind.SOURCE.extension), Kind.SOURCE);
+        this.code = code;
+    }
+    
+    @Override
+    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+        return code;
+    }
+}
