6adc3b4202e180ce40116c68cf95af7e9827b9a1
shobizz <shobizz@uw.edu>
2017-10-21 22:25:34 -0700

    player

--- src/Input.java
+++ src/Input.java
@@ -1,4 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Input {
 
+    static String s = "public class StayWithUs { \n\n\tPlayer p; \n\n\tpublic void run(String useless) { \n\t\tsetup();\n\t\taction(p);\n\t}\n\n\t"
+             + "public void setup() {\n\t\t";
+    static String end = "\n\t}\n}";
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        test();
+        s = s + end;
+        System.out.println(s);
+    }
+    
+    public static void test() throws FileNotFoundException {
+        File inputFile = new File("C:\\Users\\syjai\\git\\StayWithUs\\src\\player.txt");
+        Scanner scan = new Scanner(inputFile);
+        String line;
+        String fline;
+        while (scan.hasNext()) {
+                line = scan.next();
+                fline = s + line + "\n\t\t";
+                s = fline; 
+        }
+    }
 }
+
+
